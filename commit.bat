adc98567e6a9f78f190817b0873fa6930b67a56d
Ace Daksh <ektadaksh7@gmail.com>
2022-08-02 18:35:00 -0700

    It accomplishes nothing.

---
+++ commit.bat
@@ -0,0 +1,13 @@
+@echo off
+git add .
+FOR /F "tokens=* USEBACKQ" %%F IN (`gum choose "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert"`) DO (
+SET type=%%F
+)
+FOR /F "tokens=* USEBACKQ" %%F IN (`gum input --value %type%": " --placeholder "Summary of change"`) DO (
+SET msg=%%F
+)
+FOR /F "tokens=* USEBACKQ" %%F IN (`gum write --placeholder "Description of change"`) DO (
+SET desc=%%F
+)
+gum confirm "Commit changes?" && git commit -m "%summary%" -m "%desc%"
+
